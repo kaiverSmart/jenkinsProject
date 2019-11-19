c2b2737c8a542c974725156cbddc86b4e31a152b
kaiverSmart <925701025@qq.com>
2019-11-19 16:06:47 +0800

    project init

---
+++ fastlaneTest/fastlaneTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  fastlaneTest
+//
+//  Created by OpenLee on 2019/11/19.
+//  Copyright © 2019 OpenLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+    }
+
+
+}
+
