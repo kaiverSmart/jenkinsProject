13ab667c155599d617e6cc9c87a8576dc955c917
kaiverSmart <925701025@qq.com>
2019-11-19 18:16:59 +0800

    开始打包

--- fastlaneTest/fastlaneTest/ViewController.swift
+++ fastlaneTest/fastlaneTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view发发发
         self.view.backgroundColor = UIColor.red
     }
 
