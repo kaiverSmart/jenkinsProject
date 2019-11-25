e561020ff677c78a4a6bc0b898fe8ba58399c313
kaiverSmart <925701025@qq.com>
2019-11-25 18:39:43 +0800

    sunnry_Ngrok  hook Test

--- fastlaneTest/fastlaneTest/ViewController.swift
+++ fastlaneTest/fastlaneTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the v
+        // Do any additional setup after loading the vddd
         self.view.backgroundColor = UIColor.red
     }
 
