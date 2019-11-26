cba948e0b9cfc30c77b22ed8b817aa8f6314b867
kaiverSmart <925701025@qq.com>
2019-11-26 18:05:35 +0800

    remote  tijao

--- fastlaneTest/fastlaneTest/ViewController.swift
+++ fastlaneTest/fastlaneTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //you know nothing bbb
+        //you know nothing bbb remote
         self.view.backgroundColor = UIColor.red
     }
 
