737165fc3dfc34c7cac924654a63be9f7e21cde4
kaiverSmart <925701025@qq.com>
2019-11-26 17:54:54 +0800

    ok

--- fastlaneTest/fastlaneTest/ViewController.swift
+++ fastlaneTest/fastlaneTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //keyilema
         self.view.backgroundColor = UIColor.red
     }
 
