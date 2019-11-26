6da8d182ac3f557d7f067a59591976a4c189f118
kaiverSmart <925701025@qq.com>
2019-11-26 17:59:41 +0800

    bbb

--- fastlaneTest/fastlaneTest/ViewController.swift
+++ fastlaneTest/fastlaneTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //you know nothing
+        //you know nothing bbb
         self.view.backgroundColor = UIColor.red
     }
 
